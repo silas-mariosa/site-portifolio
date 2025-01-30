bd3dd6e500a77976beea3c718b0b9fc36d4d0bf8
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2025-01-30 00:57:00 -0300

    chore: add portifolio card

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import Atributos from "@/components/atributos";
 import BannerMid from "@/components/bannerMid";
 import BannerTop from "@/components/bannerTop";
 import Language from "@/components/language";
+import Portifolios from "@/components/portifolios";
 import AppSidebar from "@/components/sideBar";
-
-import { Sidebar, SidebarContent, SidebarProvider } from "@/components/ui/sidebar";
-
 
 export default function Home() {
   return (
@@ -20,6 +18,7 @@ export default function Home() {
       <BannerTop />
       <BannerMid />
       <Atributos />
+      <Portifolios />
     </div>
   );
 }
